c88757b9c64d923390313443aa1921b706c93229
0BIG-BANG0 <96254268+0BIG-BANG0@users.noreply.github.com>
2024-04-17 12:17:34 +0530

    Lecture 1 done

--- 1_Intro React/index.js
+++ 1_Intro React/index.js
@@ -5,9 +5,14 @@
 // document.getElementById('root').appendChild(heading)
 // console.log("Javascript Header",heading)
 
-/** REACT */
+/** REACT with JS*/
 //create Element
-const reactHeading = React.createElement("h1", {className : "head",id:"reactHead", children:"Hello React!"})
-console.log("React Header : ",reactHeading)
-//append Element
-ReactDOM.createRoot(document.getElementById('root')).render(reactHeading)
+// const reactHeading = React.createElement("h1", {className : "head",id:"reactHead", children:"Hello React!"})
+// console.log("React Header : ",reactHeading)
+// //append Element
+
+//**React with JSX */
+const jsxHeading  = <><h1 className="head"> Hello JSX </h1>
+<p>This is created with JSX</p>
+</>
+ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading)
